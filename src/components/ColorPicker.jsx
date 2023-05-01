7d971b2438cd8416f1ecb6c6af4887da55afd047
Jesus <jesusg.1301.jg@gmail.com>
2023-04-30 23:32:30 -0400

    created the color picker component

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {SketchPicker} from 'react-color'
+import { useSnapshot } from 'valtio'
+import state from '../store'
+
+
 
 const ColorPicker = () => {
+  const snap = useSnapshot(state)
   return (
-    <div>ColorPicker</div>
+    <div className='absolute left-[100%] ml-3'>
+      <SketchPicker
+      color={snap.color} 
+      disableAlpha
+      onChange={(color) => state.color = color.hex}
+      />
+    </div>
   )
 }
 
