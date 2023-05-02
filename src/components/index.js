43e61baa90c36b48a25ef81430df1823734b41d6
Jesus <jesusg.1301.jg@gmail.com>
2023-05-01 22:10:49 -0400

    fix deployment

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import CustomButton from "./CustomButton";
 import AIPicker from "./AIPicker";
 import ColorPicker from "./ColorPicker";
 import FilePicker from "./FilePicker";
-import Tab from "./tab";
+import Tab from "./Tab";
 export {
     CustomButton,
     AIPicker,
