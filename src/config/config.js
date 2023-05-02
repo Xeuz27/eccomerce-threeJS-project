72fd3b0488a9615f5624a01e055f7618ab891155
Jesus <jesusg.1301.jg@gmail.com>
2023-05-01 22:08:29 -0400

    changed url to be production url of backend api

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ const config = {
     backendUrl: "http://localhost:8080/api/v1/dalle",
   },
   production: {
-    backendUrl: "https://devswag.onrender.com/api/v1/dalle",
+    backendUrl: "https://dall-e-generating-images-api.onrender.com/api/v1/dalle",
   },
 };
 
--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -59,7 +59,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true)
 
-      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+      const response = await fetch(config.production.backendUrl, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
