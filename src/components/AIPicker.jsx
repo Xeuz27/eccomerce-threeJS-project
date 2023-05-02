db703f35f9cefaf6df9b7141f7ae2c7b85d557d2
Jesus <jesusg.1301.jg@gmail.com>
2023-05-01 21:42:22 -0400

    created ai picker, to accept prompts to generate logo or textures for the shirt canvas

--- src/components/AIPicker.jsx
+++ src/components/AIPicker.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import CustomButton from "./CustomButton";
 
-const AIPicker = () => {
+const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
   return (
-    <div>AIPicker</div>
-  )
-}
+    <div className="aipicker-container">
+      <textarea
+        placeholder="Ask Ai"
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        className="aipicker-textarea"
+      />
+      <div className="flex flex-wrap gap-3">
+        {generatingImg ? (
+          <CustomButton
+            type="outline"
+            title="Asking AI..."
+            customStyles="text-xs"
+          />
+        ) : (
+          <>
+            <CustomButton
+              type='outline'
+              title='Ai Logo'
+              handleClick={()=> handleSubmit('logo')}
+              customStyles='text-xs'
+            />
+            <CustomButton
+              type='filled'
+              title='Ai Full'
+              handleClick={()=> handleSubmit('full')}
+              customStyles='text-xs'
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default AIPicker
+export default AIPicker;
